df50cdba8a085d1a5c5d4aad9bc5fcb53f84b3f4
Divya <divyaakshay011@gmail.com>
2026-03-22 17:12:51 +0530

    added ping-pong problem

--- Explicit Locks/src/Main.java
+++ Explicit Locks/src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import reentrantlock.PingPong;
 
+public class Main {
+    public static void main(String[] args)  {
+
+        PingPong pp = new PingPong();
+        Thread t1 = new Thread(
+                () -> {
+                    try {
+                        pp.ping();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        );
+        Thread t2 = new Thread( () -> {
+            try {
+                pp.pong();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        t1.start();
+        t2.start();
     }
 }
--- Explicit Locks/src/reentrantlock/PingPong.java
+++ Explicit Locks/src/reentrantlock/PingPong.java
@@ -1,4 +1,46 @@
 package reentrantlock;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class PingPong {
+
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition pingTurn = lock.newCondition();
+    private final Condition pongTurn = lock.newCondition();
+    private boolean isPingTurn = true;
+
+    public void ping() throws InterruptedException {
+        for (int i=0;i<5;i++){
+            lock.lock();
+
+            try{
+                while (!isPingTurn){
+                    pingTurn.await();
+                }
+                System.out.println("Ping");
+                isPingTurn = false;
+                pongTurn.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public void pong() throws InterruptedException {
+        for (int i=0;i<5;i++){
+            lock.lock();
+
+            try{
+                while (isPingTurn){
+                    pongTurn.await();
+                }
+                System.out.println("Pong");
+                isPingTurn = true;
+                pingTurn.signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
 }
